19f0f1770ce33d500084eb1a491af4b09fefaf11
daiki8 <daiki1945815@gmail.com>
2023-04-03 00:16:29 +0900

    feat: add chatAI to the app

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,21 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+// OpenAI API
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API"
-    });
+  const message = req.body.message
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{
+      role: "user", content: message
+    }],
+  });
+
+  context.res.json({
+      text: completion.data.choices[0].message.content
+  });
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Chat {
+  message: string;
+  isUser: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  template: `<div>{{message}}</div>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   message = '';
+  chats: Chat[] = [];
 
-  constructor(private http: HttpClient) {
-    this.http.get('/api/message')
-      .subscribe((resp: any) => this.message = resp.text);
+  constructor(private http: HttpClient) {}
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    if (!this.message.trim()) {
+      return;
+    }
+    const chat: Chat = { message: this.message, isUser: true };
+    this.chats.push(chat);
+    this.message = '';
+    const body = { message: chat.message };
+    this.http.post('/api/message', body).subscribe((resp: any) => {
+      const chat: Chat = { message: resp.text, isUser: false };
+      this.chats.push(chat);
+    });
   }
 }
